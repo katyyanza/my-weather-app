5cc8b8732521299b196e37140f5153e706f56a19
katyyanza <154392802+katyyanza@users.noreply.github.com>
2024-03-19 21:41:49 -0400

    added on click to make the icon work as submit type

--- src/index.js
+++ src/index.js
@@ -11,10 +11,11 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
-  console.log(iconElement);
+
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-emoji" />`;
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = `; ${response.data.condition.description}`;
+
   timeElement.innerHTML = formatDate(date);
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
@@ -38,16 +39,21 @@ function formatDate(date) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
+  console.log("Setting time to " + `${day} ${hours}:${minutes}`);
   return `${day} ${hours}:${minutes}`;
 }
+
 function searchCity(city) {
   let apiKey = "33f49abe5d02797fb490bco062ft9011";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log("Searching for " + city);
   axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
-  event.preventDefault();
+  if (event) {
+    event.preventDefault();
+  }
   let searchInput = document.querySelector("#search-form-input");
 
   searchCity(searchInput.value);
